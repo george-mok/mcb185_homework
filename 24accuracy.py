4399087493f28d55129d82bc804e364b84c4947c
george-mok <sachakramo@gmail.com>
2024-01-27 13:35:20 -0800

    update

--- 24accuracy.py
+++ 24accuracy.py
@@ -4,15 +4,22 @@ import math
 import sys
 
 """
-Calculates F1 score from true positives (TP), 
-false positives (FP), and false negatives (FN)
+Calculates accuracy and F1 score from true positives (TP), 
+true negatives, (TN) false positives (FP), and false negatives (FN).
 """
 # F1 score = TP/(TP + 0.5(FP + FN))
+# Output in form: (accuracy, F1 score)
 
-def f1(TP, FP, FN):
-	assert(TP > 0)
+def f1(TP, FP, TN, FN):
+	assert(TP >= 0)
+	assert(TN >= 0)
 	assert(FP >= 0)
 	assert(FN >= 0)
-	return (TP)/(TP + 0.5*(FP + FN))
+	acc = (TP + TN)/(TP + FP + TN + FN)
+	f = (TP)/(TP + 0.5*(FP + FN))
+	return (acc, f)
 
-print (F1(3, 2, 1))
+print(f1(3, 2, 1, 0))
+print(f1(8, 3, 4, 2))
+print(f1(3, 0, 2, 0))
+print(f1(0, 0, 0, 0))

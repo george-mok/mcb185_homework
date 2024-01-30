d8452b3ee121ed557a21e58485181cce1b1ca3e1
george-mok <sachakramo@gmail.com>
2024-01-30 09:18:34 -0800

    update

--- 22oligotemp.py
+++ 22oligotemp.py
@@ -4,9 +4,11 @@ import math
 import sys
 
 def oligo(a, c, g, t): # n = number of nt in oligo
-	n == a + c + g + t
+	n = a + c + g + t
 	if n <= 13: 	return (a + t)*2 + (c + g)*4
 	if n > 13:		return 64.9 + 41*(c + g - 16.4)/(a + c + g + t)
 
 print(oligo(5, 5, 5, 5)) 	# n > 13
 print(oligo(1, 1, 1, 1)) 	# n < 13
+print(oligo(6, 10, 12, 20))
+print(oligo(20, 10, 15, 12))

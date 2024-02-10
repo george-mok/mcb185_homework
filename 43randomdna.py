d8b2494790077a723e35425d262261a2237d18b6
george-mok <sachakramo@gmail.com>
2024-02-09 19:48:47 -0800

    update

--- 43randomdna.py
+++ 43randomdna.py
@@ -2,11 +2,11 @@
 
 import random
 
-limit = random.randint(50, 60)
 n = 3 # number of sequences
 
 for i in range(1, n+1):
 	print(f'>seq-{i}')
+	limit = random.randint(50, 60)
 	for i in range(limit):
 		print(random.choice('ACGT'), end='')
 	print()

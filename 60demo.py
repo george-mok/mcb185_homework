6959bee24e2dba5f44cd5a55007670bd9c161ef4
george-mok <sachakramo@gmail.com>
2024-02-23 17:23:51 -0800

    update

---
+++ 60demo.py
@@ -0,0 +1,78 @@
+# 60demo.py by George Mo
+
+import sys
+import mcb185
+
+"""
+for defline, seq in mcb185.read_fasta(sys.argv[1]):
+	defwords = defline.split()
+	name = defwords[0]
+	gc = 0
+	A = 0
+	C = 0
+	G = 0
+	T = 0
+	N = 0
+	for nt in seq:
+		if   nt == 'A': A += 1
+		elif nt == 'C': C += 1
+		elif nt == 'G': G += 1
+		elif nt == 'T': T += 1
+		else:           N += 1
+	print(name, A/len(seq), C/len(seq), G/len(seq), T/len(seq), N/len(seq))
+"""
+
+# List variation
+"""
+for defline, seq in mcb185.read_fasta(sys.argv[1]):
+	defwords = defline.split()
+	name = defwords[0]
+	nts = 'ACGTN'
+	counts = [0] * len(nts)
+	for nt in seq:
+		if   nt == 'A': counts[0] += 1
+		elif nt == 'C': counts[1] += 1
+		elif nt == 'G': counts[2] += 1
+		elif nt == 'T': counts[3] += 1
+		else:           counts[4] += 1
+print(name, end='\t')
+for n in counts: print(f'{n/len(seq):.4f}', end='\t')
+print()
+"""
+
+# str.find()
+"""
+for defline, seq in mcb185.read_fasta(sys.argv[1]):
+	defwords = defline.split()
+	name = defwords[0]
+	nts = 'ACGTN'
+	counts = [0] * len(nts)
+	for nt in seq:
+		idx = nts.find(nt)
+		counts[idx] += 1
+	print(name, end='\t')
+	for n in counts: print(f'{n/len(seq):.4f}', end='\t')
+	print()
+"""
+
+# str.count()
+
+"""
+for defline, seq in mcb185.read_fasta(sys.argv[1]):
+	defwords = defline.split()
+	name = defwords[0]
+	nts  = 'ACGTN'
+	print(name, end='\t')
+	for nt in nts:
+		print(f'{seq.count(nt) / len(seq):.4f}', end='\t')
+	print()
+"""
+
+# Sliding Window
+
+seq = 'ACCGTTAGCTATTCAGGGATA'
+w = 10
+s = 1
+for i in range(0, len(seq) -w +1, s):
+	subseq = seq[i:i+w]
+	print(subseq)
